333396c7adeb1d5304f2be562efe0599ba9308c3
El Amri Omar <Omar_El_Amri@protonmail.com>
2019-07-29 10:06:52 +0100

    Initial commit

---
+++ app/src/main/java/c/nav1/Controllers/Fragments/ParamsFragment.java
@@ -0,0 +1,22 @@
+package c.nav1.Controllers.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import c.nav1.R;
+
+public class ParamsFragment extends Fragment {
+
+    public static ParamsFragment newInstance() {
+        return (new ParamsFragment());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_params, container, false);
+    }
+}
